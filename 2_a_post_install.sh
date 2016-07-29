f23c3a2678119bba1da3925ad487a6e971fc8508
Abtin Forouzandeh <abtinf@gmail.com>
2016-07-28 22:36:38 -0700

    Update 2_a_post_install.sh

--- 2_a_post_install.sh
+++ 2_a_post_install.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-#update pkgfile for command no found hook
-sudo pkgfile --update
-
-#configure git
-read -p "Git username: " git_username
-git config --global user.name $git_username
-read -p "Git email: " git_email
-git config --global user.email $git_email
-git config --global core.autocrlf input
-
-#update ruby gems
-sudo gem update --system
-gem update
-
 #windows name lookup
 #sudo pacman -S --noconfirm avahi nss-mdns
 #sudo systemctl enable avahi-daemon.service
@@ -22,12 +8,6 @@ gem update
 #disable sleep on close
 #sudo bash -c "sed 's/^#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf > /etc/systemd/logind.confback; mv /etc/systemd/logind.confback /etc/systemd/logind.conf"
 #sudo bash -c "sed 's/^#HandleHibernateKey=hibernate/HandleHibernateKey=ignore/' /etc/systemd/logind.conf > /etc/systemd/logind.confback; mv /etc/systemd/logind.confback /etc/systemd/logind.conf"
-
-
-#sync clock and enable network time daemon
-#sudo pacman -S --noconfirm ntp
-#sudo ntpd -qg
-#sudo systemctl enable ntpd
 
 #AUR helper
 cd /tmp
