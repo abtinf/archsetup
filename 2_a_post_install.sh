b481e79471fd36a2f088c61c73daa01bfc4dca44
Abtin Forouzandeh <abtinf@gmail.com>
2016-07-29 13:37:34 -0700

    Update yaourt

--- 2_a_post_install.sh
+++ 2_a_post_install.sh
@@ -4,6 +4,17 @@
 sudo ip link set eth0 up
 sudo ip link set wlan0 up
 
+# AUR helper
+git clone https://aur.archlinux.org/package-query.git
+git clone https://aur.archlinux.org/yaourt.git
+cd package-query
+makepkg -si
+cd ../yaourt
+makepkg -si
+cd ..
+rm -rf package-query
+rm -rf yaourt
+
 #windows name lookup
 #sudo pacman -S --noconfirm avahi nss-mdns
 #sudo systemctl enable avahi-daemon.service
@@ -12,17 +23,3 @@ sudo ip link set wlan0 up
 #disable sleep on close
 #sudo bash -c "sed 's/^#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf > /etc/systemd/logind.confback; mv /etc/systemd/logind.confback /etc/systemd/logind.conf"
 #sudo bash -c "sed 's/^#HandleHibernateKey=hibernate/HandleHibernateKey=ignore/' /etc/systemd/logind.conf > /etc/systemd/logind.confback; mv /etc/systemd/logind.confback /etc/systemd/logind.conf"
-
-#AUR helper
-cd /tmp
-wget https://aur.archlinux.org/packages/pa/package-query/package-query.tar.gz
-tar xzf package-query.tar.gz
-cd package-query
-makepkg -si
-cd ..
-cd /tmp
-wget https://aur.archlinux.org/packages/ya/yaourt/yaourt.tar.gz
-tar xzf yaourt.tar.gz
-cd yaourt
-makepkg -si
-cd ~
