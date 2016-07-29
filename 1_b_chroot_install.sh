0e0d44662549227dcf9fa41cfd2eb269029ae2e3
Abtin Forouzandeh <abtinf@gmail.com>
2016-07-29 16:06:12 -0700

    Add pauses

--- 1_b_chroot_install.sh
+++ 1_b_chroot_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+read -n1 -r -p "Press any key to continue..." key
 # Set time zone
 read -p "Timezone (default /America/Los_Angeles): " timezone
 timezone=${timezone:-/America/Los_Angeles}
 ln -s "/usr/share/zoneinfo"$timezone /etc/localtime
 hwclock --systohc --utc
 
+read -n1 -r -p "Press any key to continue..." key
 # Set language
 echo "Setting locale to en_US.UTF-8"
 mv /etc/locale.gen /etc/locale.gen.backup
@@ -13,28 +15,34 @@ sed 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen.backup > /etc/loc
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 locale-gen
 
+read -n1 -r -p "Press any key to continue..." key
 # Set hostname
 read -p "Hostname: " hostname
 echo $hostname > /etc/hostname
 # TODO add hostname to hosts file
 
+read -n1 -r -p "Press any key to continue..." key
 # Set root password
 echo "Enter a root password (root will also be disabled)"
 passwd
 passwd -l root
 
+read -n1 -r -p "Press any key to continue..." key
 # Add user
 read -p "Username: " username
 useradd -m -G wheel,storage -s /bin/bash $username
 passwd $username
 
+read -n1 -r -p "Press any key to continue..." key
 # Enable wheel group for sudo
 mv /etc/sudoers /etc/sudoers.backup
 sed 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers.backup > /etc/sudoers
 
+read -n1 -r -p "Press any key to continue..." key
 # Sudo password caching
 echo Defaults timestamp_timeout=20 >> /etc/sudoers
 
+read -n1 -r -p "Press any key to continue..." key
 # Install useful packages
 # Essential
 pacman -S --noconfirm base-devel intel-ucode grub
@@ -55,11 +63,13 @@ pacman -S --noconfirm libdvdread libdvdcss libdvdnav libcdio vlc youtube-dl
 # Productivity
 pacman -S --noconfirm texlive-most zim audacity inkscape gimp gnucash tigervnc rdesktop libreoffice-still filezilla
 
-
+read -n1 -r -p "Press any key to continue..." key
 # Pull scripts
 pacman -S --noconfirm git
 cd /home/$username
 su - $username -c 'git clone https://github.com/abtinf/archsetup.git'
+
+read -n1 -r -p "Press any key to continue..." key
 # Create config symlinks
 su - $username -c 'ln -s ./archsetup/config/.Xresources    ~/.Xresources'
 su - $username -c 'ln -s ./archsetup/config/.xinitrc       ~/.xinitrc'
@@ -69,6 +79,7 @@ su - $username -c 'ln -s ./archsetup/config/.spectrwm.conf ~/.spectrwm.conf'
 su - $username -c 'ln -s ./archsetup/config/.conkyrc       ~/.conkyrc'
 su - $username -c 'ln -s ./archsetup/config/.vimrc         ~/.vimrc'
 
+read -n1 -r -p "Press any key to continue..." key
 # External storage manager
 systemctl enable udisks2.service
 systemctl enable devmon@$username.service
@@ -76,10 +87,12 @@ chmod -s /usr/bin/udevil
 cp ./archsetup/config/50-udiskie.rules /etc/polkit-1/rules.d/
 cp ./archsetup/config/99-udisks2.rules /etc/udev/rules.d/
 
+read -n1 -r -p "Press any key to continue..." key
 # Enable display manager
 systemctl enable xdm-archlinux.service
 chmod +x /home/$username/.xinitrc
 
+read -n1 -r -p "Press any key to continue..." key
 # Disable dumb network device naming
 ln -s /dev/null /etc/udev/rules.d/80-net-setup-link.rules
 # Enable netctl
@@ -90,9 +103,11 @@ ufw enable
 ufw default deny
 systemctl enable ufw
 
+read -n1 -r -p "Press any key to continue..." key
 # Update pkgfile for command no found hook
 pkgfile --update
 
+read -n1 -r -p "Press any key to continue..." key
 # Configure git
 read -p "Git username: " git_username
 su - $username -c 'git config --global user.name $git_username'
@@ -100,19 +115,23 @@ read -p "Git email: " git_email
 su - $username -c 'git config --global user.email $git_email'
 su - $username -c 'git config --global core.autocrlf input'
 
+read -n1 -r -p "Press any key to continue..." key
 # Update ruby gems
 gem update --system
 su - $username -c 'gem update'
 
+read -n1 -r -p "Press any key to continue..." key
 # Sync clock and enable network time daemon
 ntpd -qg
 systemctl enable ntpd
 
+read -n1 -r -p "Press any key to continue..." key
 # Add encryption hook to mkinitcpio and generate
 mv /etc/mkinitcpio.conf /etc/mkinitcpio.conf.backup
 sed 's/ filesystems/ encrypt filesystems/g' /etc/mkinitcpio.conf.backup > /etc/mkinitcpio.conf
 mkinitcpio -p linux
 
+read -n1 -r -p "Press any key to continue..." key
 # Install grub
 read -p "Path to device (default /dev/sda): " dev_path
 dev_path=${dev_path:-/dev/sda}
@@ -121,6 +140,7 @@ sed 's#^GRUB_CMDLINE_LINUX=""#GRUB_CMDLINE_LINUX="cryptdevice='$dev_path'2:crypt
 grub-install $dev_path
 grub-mkconfig -o /boot/grub/grub.cfg
 
+read -n1 -r -p "Press any key to continue..." key
 # Leave chroot
 exit
 
